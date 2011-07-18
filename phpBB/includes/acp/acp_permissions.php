30abf1250211343ea8bd0f724c39f88a05f2dcfa
Daniel Lindenberger <daniel@action-ed.com>
2011-07-18 09:42:02 -0700

    [ticket/10124] Remove `$_POST` dependency from acp_permissions
    The `set_permission` method is dependent upon the $_POST array which
    makes it impossible to call that method with the data that has to be
    set. This commit introduces a new method parameter which can be used to
    pass the information to the method.
    
    The old referances to the `$_POST` array and `request_var` calls are
    still in place for backward compatibility but could be removed in the
    develop branch.
    
    PHPBB3-10124

--- phpBB/includes/acp/acp_permissions.php
+++ phpBB/includes/acp/acp_permissions.php
@@ -655,17 +655,23 @@ class acp_permissions
 	/**
 	* Apply permissions
 	*/
-	function set_permissions($mode, $permission_type, &$auth_admin, &$user_id, &$group_id)
+	function set_permissions($mode, $permission_type, &$auth_admin, &$user_id, &$group_id, $scripted = false)
 	{
 		global $user, $auth;
-
-		$psubmit = request_var('psubmit', array(0 => array(0 => 0)));
+        if (is_array($scripted)) 
+        {
+    		$psubmit = $scripted['psubmit'];
+        } 
+        else 
+        {
+    		$psubmit = request_var('psubmit', array(0 => array(0 => 0)));
+        }
 
 		// User or group to be set?
 		$ug_type = (sizeof($user_id)) ? 'user' : 'group';
 
 		// Check the permission setting again
-		if (!$auth->acl_get('a_' . str_replace('_', '', $permission_type) . 'auth') || !$auth->acl_get('a_auth' . $ug_type . 's'))
+		if (!is_array($scripted) && (!$auth->acl_get('a_' . str_replace('_', '', $permission_type) . 'auth') || !$auth->acl_get('a_auth' . $ug_type . 's')))
 		{
 			trigger_error($user->lang['NO_AUTH_OPERATION'] . adm_back_link($this->u_action), E_USER_WARNING);
 		}
@@ -676,7 +682,7 @@ class acp_permissions
 		list($ug_id, ) = each($psubmit);
 		list($forum_id, ) = each($psubmit[$ug_id]);
 
-		if (empty($_POST['setting']) || empty($_POST['setting'][$ug_id]) || empty($_POST['setting'][$ug_id][$forum_id]) || !is_array($_POST['setting'][$ug_id][$forum_id]))
+		if (!is_array($scripted) && (empty($_POST['setting']) || empty($_POST['setting'][$ug_id]) || empty($_POST['setting'][$ug_id][$forum_id]) || !is_array($_POST['setting'][$ug_id][$forum_id])))
 		{
 			trigger_error('WRONG_PERMISSION_SETTING_FORMAT', E_USER_WARNING);
 		}
@@ -684,13 +690,36 @@ class acp_permissions
 		// We obtain and check $_POST['setting'][$ug_id][$forum_id] directly and not using request_var() because request_var()
 		// currently does not support the amount of dimensions required. ;)
 		//		$auth_settings = request_var('setting', array(0 => array(0 => array('' => 0))));
-		$auth_settings = array_map('intval', $_POST['setting'][$ug_id][$forum_id]);
+
+        if (is_array($scripted)) 
+        {
+    		$auth_settings = array_map('intval', $scripted['setting'][$ug_id][$forum_id]);
+        } 
+        else 
+        {
+    		$auth_settings = array_map('intval', $_POST['setting'][$ug_id][$forum_id]);
+        }
+
 
 		// Do we have a role we want to set?
-		$assigned_role = (isset($_POST['role'][$ug_id][$forum_id])) ? (int) $_POST['role'][$ug_id][$forum_id] : 0;
+        if (is_array($scripted)) 
+        {
+    		$assigned_role = (isset($scripted['role'][$ug_id][$forum_id])) ? (int) $scripted['role'][$ug_id][$forum_id] : 0;
+        } 
+        else 
+        {
+    		$assigned_role = (isset($_POST['role'][$ug_id][$forum_id])) ? (int) $_POST['role'][$ug_id][$forum_id] : 0;
+        }
 
 		// Do the admin want to set these permissions to other items too?
-		$inherit = request_var('inherit', array(0 => array(0)));
+        if (is_array($scripted)) 
+        {
+    		$inherit = $scripted['inherit'];
+        } 
+        else 
+        {
+    		$inherit = request_var('inherit', array(0 => array(0)));
+        }
 
 		$ug_id = array($ug_id);
 		$forum_id = array($forum_id);
@@ -738,7 +767,14 @@ class acp_permissions
 
 		$this->log_action($mode, 'add', $permission_type, $ug_type, $ug_id, $forum_id);
 
-		trigger_error($user->lang['AUTH_UPDATED'] . adm_back_link($this->u_action));
+        if (is_array($scripted)) 
+        {
+            return true;
+        } 
+        else 
+        {
+    		trigger_error($user->lang['AUTH_UPDATED'] . adm_back_link($this->u_action));
+        }
 	}
 
 	/**
