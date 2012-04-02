04dce3d68c82c3f9fe621625afa66685a6e9d5f8
Erik Frèrejean <erikfrerejean@phpbb.com>
2012-04-02 11:57:25 +0200

    [feature/ledges-2] Add `core.page_footer_override`
    Add a ledge that will allow listeners to override the build in
    `page_footer` function.
    http://area51.phpbb.com/phpBB/viewtopic.php?f=111&t=42741&p=237037
    
    PHPBB3-9550

--- phpBB/includes/functions.php
+++ phpBB/includes/functions.php
@@ -4800,7 +4800,17 @@ function page_header($page_title = '', $display_online_list = true, $item_id = 0
 function page_footer($run_cron = true)
 {
 	global $db, $config, $template, $user, $auth, $cache, $starttime, $phpbb_root_path, $phpEx;
-	global $request;
+	global $request, $phpbb_dispatcher;
+
+	// A listener can set this variable to `true` when it overrides this function
+	$page_footer_override = false;
+
+	$vars= array('run_cron', 'page_footer_override');
+	extract($phpbb_dispatcher->trigger_event('core.page_footer_override', compact($vars)));
+	if ($page_footer_override)
+	{
+		return;
+	}
 
 	// Output page creation time
 	if (defined('DEBUG'))
